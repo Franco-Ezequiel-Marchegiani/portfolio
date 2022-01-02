d405e8c3d3c8e569643edd071689da2661635506
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-01 22:11:59 -0300

    El próximo paso es hacer el Header Sticky

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -4,6 +4,23 @@ import { Link } from 'react-router-dom';
 import Logo from '../../images/icons/react-dark.png'
 import './navBar.css';
 function NavBar() {
+  /*
+  Ver el tema del NavBar Sticky 
+  document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar_top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar_top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+ */
   return (
     <nav className='navBar-Container'>
       <Navbar bg="light" expand="lg">
