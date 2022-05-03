f7e558a1e8955f6f5ef0b8d4f17ba61e226a17dd
Franco Ezequiel <eze.cavs@hotmail.com>
2022-05-03 16:11:32 -0300

    Set the image

--- src/components/proyectDetail/ProyectDetail.js
+++ src/components/proyectDetail/ProyectDetail.js
@@ -102,6 +102,7 @@ function ProyectDetail(props) {
       parrafo1: "Esta página se creó utilizando React Js en su funcionamiento, y se utilizó Firebase para subirlo a un servidor, el sketch del diseño fue brindado por Dev-Challenges, el cual me encargué de plasmarlo al código en detalle. La página también realiza un llamado hacia una API para recolectar la información de cada puesto de trabajo.",
       imagen2: quizAppSecond,
       parrafo2: "La página consiste en mostrar trabajos disponibles, cuenta con dos barras de búsquedas completamente funcionales, y también un filtro para aquellos trabajos que son Full-Time o de tiempo completo",
+      imagen3: quizAppThird,
       parrafo3: "También, cuenta con la vista del detalle del puesto de trabajo el cual cuenta en detalle información del puesto, qué requisitos se necesita, los beneficios que incluye, etc. En esta sección es información 100% recolectada de la API la cual utilicé para dicho proyecto"
     }
   ]
