decdba0543feac00fd70889351bd82d891e0a82b
Franco Ezequiel <eze.cavs@hotmail.com>
2022-05-03 16:10:25 -0300

    Import image from the quiz app

--- src/components/proyectDetail/ProyectDetail.js
+++ src/components/proyectDetail/ProyectDetail.js
@@ -21,6 +21,8 @@ import GithubJobsSecond from '../../images/proyectos/githubJobs/githubJobs1.png'
 import GithubJobsThird from '../../images/proyectos/githubJobs/githubJobs2.png';
 
 import quizAppHome from '../../images/proyectos/quizApp/home.png';
+import quizAppSecond from '../../images/proyectos/quizApp/home1.png';
+
 
 import AOS from 'aos'
 function ProyectDetail(props) {
